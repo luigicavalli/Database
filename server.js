c7e26d59ba9fcc8702e3baedb1b6ae0831763a91
Luigi Cavalli <luigi_cavalli@outlook.com>
2024-10-08 18:17:11 +0200

    added get and post requests

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ const app = express();
 const port = 3000;
 
 app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/info', (res) => {
+  res.status(200).json({ message: 'Success!' });
+});
+
+app.post('/login', (req, res) => {
+  const token = 'hdjdjbdkdw';
+
+  const { email, password } = req.body;
+
+  res.status(200).json({
+    message: 'Success!',
+    apiToken: token,
+    email: email,
+    password: password,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
