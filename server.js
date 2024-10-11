764f7948c454fffe9a5bf69c2ddf8bea0fd9e9ed
Luigi Cavalli <luigi_cavalli@outlook.com>
2024-10-11 19:01:09 +0200

    connected frontend app to backend database

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import mysql from 'mysql';
+import 'dotenv/config';
 
 const app = express();
 const port = 3000;
@@ -13,18 +14,35 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error(error.message);
+    return;
+  }
+  console.log('Database connected');
+});
+
 app.get('/info', (req, res) => {
   res.status(200).json({ message: 'Success!' });
 });
 
+const secretKey = process.env.SECRET_KEY;
+
 app.post('/login', (req, res) => {
-  const token = 'hdjdjbdkdw';
+  const jwtToken = jwt.sign({ userID: 10 }, secretKey, { expiresIn: '1h' });
 
   const { email, password } = req.body;
 
   res.status(200).json({
     message: 'Success!',
-    apiToken: token,
+    apiToken: jwtToken,
     email: email,
     password: password,
   });
@@ -40,10 +58,11 @@ app.post('/sign-up', async (req, res) => {
   if (cryptedPassword !== null) {
     res.status(200).json({
       message: 'success',
+      apiToken: jwtToken,
       name: name,
       surname: surname,
       email: email,
-      password: cryptedPassword??`not found`,
+      password: cryptedPassword ?? `not found`,
     });
   }
 });
